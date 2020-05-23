e27d5838faebd4bc64e3397ee45665a1f6b5f688
Pavan <pavanpabs@gmail.com>
2020-05-23 10:53:46 +0530

    Cart Route changed

--- Routings/CartRoutes.js
+++ Routings/CartRoutes.js
@@ -1,10 +1,10 @@
 const express=require('express');
 const router=express.Router();
 const Cart = require('../schema/Cart');
-const auth = require("../Authentication/Auth");
+//const auth = require("../Authentication/Auth");
 const StoreProducts = require('../schema/StoreManagerProducts');
 
-router.post('/newCart',auth, function (req, res, next) {
+router.post('/newCart', function (req, res, next) {
     Cart.create({
         
         user: req.body.user,
@@ -27,33 +27,9 @@ router.post('/newCart',auth, function (req, res, next) {
  
   router.get('/carts/:Uid', async function (req, res, next) {
     const userId=req.params.Uid;
-    let pIds;
-    let userCount ;
-    pIds = await Cart.find({user:userId}).then(function (item) {
-      return item;
+     Cart.find({user:userId}).then(function (item) {
+      res.send(item);
     });
-    console.log(pIds);
-    // userCount = await Register.find({}).then(function (item) {
-    //   return item.length;
-    // });
-  
-    // productCount = await Product.find({}).then(function (item) {
-    //   return item.length;
-    // });
-  
-    // storemanagerCount = await Register.find({ utype: "sm" }).then(function (
-    //   item
-    // ) {
-    //   return item.length;
-    // });
-  
-    // res.json({
-    //   categories: cateCount,
-    //   users: userCount,
-    //   products: productCount,
-    //   storemanagers: storemanagerCount,
-    // });
-  });
 
-
+});
   module.exports=router;
